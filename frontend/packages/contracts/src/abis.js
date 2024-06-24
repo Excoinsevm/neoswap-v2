e2a5ba9ae926f89fcceef3225cbe8fd292d2b110
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-24 04:43:11 +0100

    Update abis.js

--- frontend/packages/contracts/src/abis.js
+++ frontend/packages/contracts/src/abis.js
@@ -1,7 +1,7 @@
 import erc20 from './abis/erc20.json'
 import factory from './abis/factory.json'
 import pair from './abis/pair.json'
-import router01 from './abis/router01.json'
+import router01 from './abis/UniswapV2Router01.json'
 import router02 from './abis/router02.json'
 
 export default {
